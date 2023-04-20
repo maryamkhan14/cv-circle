53b0f0bfd01271baa598d9924b480b5a5543dd48
Maryam Khan <kmaryam818@gmail.com>
2023-04-19 18:03:14 -0700

    Set up tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App bg-slate-500 text-yellow-400 w-screen h-screen"></div>
+  );
 }
 
 export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    colors: { ...colors },
+    extend: {},
+  },
+  plugins: [],
+};
