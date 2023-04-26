616f2576505c26a68e4c1670933346bdc0644f51
Maryam Khan <kmaryam818@gmail.com>
2023-04-26 11:50:12 -0700

    Completed PostForm input components

--- src/components/PostForm.jsx
+++ src/components/PostForm.jsx
@@ -3,33 +3,82 @@ import { useEffect, useState, useContext } from "react";
 import { UserContext } from "../context/UserContext";
 
 const PostForm = () => {
+  const [post, setPost] = useState({});
+  const [title, setTitle] = useState("");
+  const [postContent, setPostContent] = useState("");
+  const [attachment, setAttachment] = useState({});
+  const [attachmentError, setAttachmentError] = useState("");
+
+  const createPost = (e) => {
+    e.preventDefault();
+    if (attachment.type != "application/pdf") {
+      setAttachmentError("Error: Please only attach PDFs.");
+    } else {
+      setPost({
+        title: title,
+        postContent: postContent,
+        attachment: attachment,
+      });
+    }
+  };
+  useEffect(() => console.log(attachment), [attachment]);
   return (
     <div className="flex justify-center items-center w-full h-full">
       <div className="flex flex-col w-5/6 h-5/6 m-3 gap-5">
         <h1 className="text-5xl">Create a post</h1>
         <form className="rounded flex shadow-md flex-col gap-5 border p-3">
           <span className="flex flex-col md:flex-row gap-2 m-2 justify-center items-center">
-            <label htmlFor="title">Title</label>
+            <label htmlFor="title" className="font-medium">
+              Title
+            </label>
             <input
               className="border border-slate-800 w-full p-2 rounded"
               type="text"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
               required="required"
             />
           </span>
           <span className="flex flex-col md:flex-row gap-2 m-2 justify-center items-center">
-            <label htmlFor="post">Post</label>
+            <label htmlFor="post-content" className="font-medium">
+              Post
+            </label>
             <textarea
-              id="post"
-              name="post"
+              id="post-content"
+              name="post-content"
               rows="4"
               cols="50"
+              value={postContent}
+              onChange={(e) => setPostContent(e.target.value)}
               className="border border-slate-800 w-full p-2 rounded"
             ></textarea>
+          </span>
+          <span className="flex flex-col md:flex-row gap-2 m-2 justify-center items-center">
+            <label
+              htmlFor="attachment"
+              className="font-medium text-slate-50 bg-amber-800 hover:bg-amber-800/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 rounded-lg px-5 py-2.5 flex items-center justify-center"
+            >
+              Attach Your Resume
+            </label>
+
+            <input
+              className="hidden"
+              type="file"
+              name="attachment"
+              id="attachment"
+              accept=".pdf"
+              onChange={(e) => setAttachment(e.target.files[0])}
+            />
+            {attachment && <p className="self-center">{attachment.name}</p>}
+            {attachmentError && (
+              <p className="font-bold text-amber-800">{attachmentError}</p>
+            )}
           </span>
           <span className="flex self-center">
             <button
               type="button"
               className="text-slate-50 bg-amber-800 hover:bg-amber-800/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg px-5 py-2.5 flex items-center justify-center mr-2 mb-2"
+              onClick={createPost}
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
