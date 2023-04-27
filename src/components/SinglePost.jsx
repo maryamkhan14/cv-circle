cb7edd959f178a58f357ff5dbd54f4ef8a2f7dbc
Maryam Khan <kmaryam818@gmail.com>
2023-04-26 22:43:31 -0700

    Added deleting functionality

--- src/components/SinglePost.jsx
+++ src/components/SinglePost.jsx
@@ -3,12 +3,13 @@ import { useEffect, useState, useContext } from "react";
 import { UserContext } from "../context/UserContext";
 
 import { supabase } from "../client";
-import { Link } from "react-router-dom";
-import { useParams } from "react-router-dom";
+import { Link, useParams, useNavigate } from "react-router-dom";
 
 const SinglePost = () => {
   const { user } = useContext(UserContext);
   const { id } = useParams();
+  const navigate = useNavigate();
+
   const [post, setPost] = useState({});
   const getPost = async () => {
     const { data, errors } = await supabase.from("posts").select().eq("id", id);
@@ -20,6 +21,7 @@ const SinglePost = () => {
   };
   const increaseUpvotes = async () => {
     if (Object.keys(user).length > 0) {
+      //TODO: Add error handling
       const { data: upvotedPost, errors } = await supabase
         .from("posts")
         .update({ upvotes: post.upvotes + 1 })
@@ -28,6 +30,16 @@ const SinglePost = () => {
 
       setPost(...upvotedPost);
     }
+  };
+
+  const deletePost = async () => {
+    const { data, errors } = await supabase.from("posts").delete().eq("id", id);
+    if (errors) {
+      console.log(errors);
+    } else {
+      console.log(data);
+    }
+    navigate("/");
   };
 
   useEffect(() => {
@@ -58,28 +70,36 @@ const SinglePost = () => {
               Note: To see the image in full size, click it.
             </p>
 
-            <span
-              className={`${
-                Object.keys(user).length > 0 && "hover:cursor-pointer"
-              } flex gap-2 border p-3 w-full md:w-auto md:min-w-[15%] md:max-w-[20%] rounded-full bg-amber-800/70 justify-center items-center`}
-              onClick={increaseUpvotes}
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                className="w-6 h-6 stroke-slate-50"
+            <div className="w-full flex gap-2">
+              <span
+                className={`${
+                  Object.keys(user).length > 0 && "hover:cursor-pointer"
+                } flex gap-2 border p-3 md:w-auto md:min-w-[15%] md:max-w-[20%] rounded-lg bg-amber-800 justify-center items-center`}
+                onClick={increaseUpvotes}
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M12 6v12m-3-2.818l.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  className="w-6 h-6 stroke-slate-50"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M12 6v12m-3-2.818l.879.659c1.171.879 3.07.879 4.242 0 1.172-.879 1.172-2.303 0-3.182C13.536 12.219 12.768 12 12 12c-.725 0-1.45-.22-2.003-.659-1.106-.879-1.106-2.303 0-3.182s2.9-.879 4.006 0l.415.33M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
 
-              <p className="text-slate-50">{post && post.upvotes} upvotes</p>
-            </span>
+                <p className="text-slate-50">{post && post.upvotes} upvotes</p>
+              </span>
+              <button
+                className="text-slate-50 bg-red-500 hover:bg-red-500/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg px-5 py-2.5 flex items-center justify-center"
+                onClick={deletePost}
+              >
+                Delete
+              </button>
+            </div>
           </>
         )}
       </div>
