5d2aa7c4a6e7c2cb1ae089cbbd43342a3da97968
Maryam Khan <kmaryam818@gmail.com>
2023-06-03 18:20:59 -0700

    Removed unnecessary import

--- src/components/SinglePost.jsx
+++ src/components/SinglePost.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { useEffect, useState, useContext } from "react";
 import { UserContext } from "../context/UserContext";
 import { getPost, upvotePost, checkHasUpvoted, deletePost } from "../services";
-import { supabase } from "../client";
 import { Link, useParams, useNavigate } from "react-router-dom";
 
 const SinglePost = () => {
