df1ca1b4f2a0ef87500f30ff89f418ef009ec592
Maryam Khan <kmaryam818@gmail.com>
2023-06-27 11:07:49 -0700

    Refactored handle-attachment-review test case to use fake user ID value provided by test constants resource file instead of hard-coding it

--- backend/src/apps/posts/domain/use-cases/handle-attachment-preview.spec.js
+++ backend/src/apps/posts/domain/use-cases/handle-attachment-preview.spec.js
@@ -1,4 +1,5 @@
 import { describe, expect, beforeEach, test, vi } from "vitest";
+import { FAKE_USER_ID } from "../../__test__/fixtures/constants";
 import { makeFakeImageEntity as makeFakeImage } from "../../__test__/fixtures/image";
 import makeHandleAttachmentPreview from "./handle-attachment-preview";
 
@@ -30,8 +31,7 @@ describe("Create attachment preview use case", () => {
 
   test("Successfully creates attachment preview for PDF files", async () => {
     let file = { mimetype: "application/pdf" };
-    let userId = 1;
-    let result = await handleAttachmentPreview({ file, userId });
+    let result = await handleAttachmentPreview({ file, userId: FAKE_USER_ID });
     let makePdfPreviewArgs = makePdfPreview.mock.calls[0][0];
 
     let makeImageArgs = makeImage.mock.calls[0][0];
@@ -43,7 +43,7 @@ describe("Create attachment preview use case", () => {
 
     expect(makePdfPreviewArgs).toEqual(file);
     expect(makeImageArgs.imageData).toEqual(previewImage.getImageData());
-    expect(makeImageArgs.userId).toEqual(userId);
+    expect(makeImageArgs.userId).toEqual(FAKE_USER_ID);
     expect(imagesDbInsertArgs).toEqual({
       extension: previewImage.getExtension(),
       imageData: previewImage.getImageData(),
@@ -52,9 +52,8 @@ describe("Create attachment preview use case", () => {
   });
 
   test("Successfully creates attachment preview for non-pdf files", async () => {
-    let userId = 1;
     let file = { mimetype: "image/png", data: previewImage.getImageData() };
-    let result = await handleAttachmentPreview({ file, userId });
+    let result = await handleAttachmentPreview({ file, userId: FAKE_USER_ID });
 
     let makeImageArgs = makeImage.mock.calls[0][0];
     let imagesDbInsertArgs = imagesDb.insert.mock.calls[0][0];
@@ -64,7 +63,7 @@ describe("Create attachment preview use case", () => {
     expect(imagesDb.insert).toHaveBeenCalledTimes(1);
 
     expect(makeImageArgs.imageData).toEqual(previewImage.getImageData());
-    expect(makeImageArgs.userId).toEqual(userId);
+    expect(makeImageArgs.userId).toEqual(FAKE_USER_ID);
     expect(imagesDbInsertArgs).toEqual({
       extension: previewImage.getExtension(),
       imageData: previewImage.getImageData(),
@@ -74,11 +73,12 @@ describe("Create attachment preview use case", () => {
 
   test("Throws error when database save fails", async () => {
     let file = { mimetype: "application/pdf" };
-    let userId = 1;
     imagesDb.insert.mockImplementation(async () => {
       return Promise.resolve({ error: { message: "Big test error" } });
     });
-    expect(handleAttachmentPreview({ file, userId })).rejects.toThrow(
+    expect(
+      handleAttachmentPreview({ file, userId: FAKE_USER_ID })
+    ).rejects.toThrow(
       "Error uploading image preview to database: Big test error. Post creation failed."
     );
   });
