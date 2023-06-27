701add887080409bec7a9da92235599fdc4f7c8c
Maryam Khan <kmaryam818@gmail.com>
2023-06-27 15:20:21 -0700

    Added controller that gets a single post to entry point interface

--- backend/src/apps/posts/entry-points/index.js
+++ backend/src/apps/posts/entry-points/index.js
@@ -1,11 +1,17 @@
-import { createPost, retrievePosts } from "../domain/use-cases/index.js";
+import {
+  createPost,
+  retrievePosts,
+  retrieveSinglePost,
+} from "../domain/use-cases/index.js";
 import { handleAttachmentPreview } from "../domain/use-cases/index.js";
 import makePostPost from "./post-post.js";
 import makeGetAllPosts from "./get-all-posts.js";
+import makeGetSinglePost from "./get-single-post.js";
 
 const postPost = makePostPost({ createPost, handleAttachmentPreview });
 const getAllPosts = makeGetAllPosts({ retrievePosts });
-const postController = Object.freeze({ postPost, getAllPosts });
+const getSinglePost = makeGetSinglePost({ retrieveSinglePost });
+const postController = Object.freeze({ postPost, getAllPosts, getSinglePost });
 
 export default postController;
-export { postPost, getAllPosts };
+export { postPost, getAllPosts, getSinglePost };
