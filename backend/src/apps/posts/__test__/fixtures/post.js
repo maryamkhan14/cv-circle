4a8f044d1ef3bd634d1259773377adcd1022352b
Maryam Khan <kmaryam818@gmail.com>
2023-06-27 10:33:06 -0700

    Refactored constants used in image & post test fixtures into constants resource, and commented & reordered methods for uniformity and clarity

--- backend/src/apps/posts/__test__/fixtures/post.js
+++ backend/src/apps/posts/__test__/fixtures/post.js
@@ -1,16 +1,22 @@
-const { faker } = require("@faker-js/faker");
-const { createId } = require("@paralleldrive/cuid2");
-const uniqueId = Object.freeze({
-  makeId: createId, // TODO: Add isValidId()
-});
+import {
+  FAKE_IMAGE_CDN,
+  FAKE_POST_CONTENT,
+  FAKE_POST_TITLE,
+  FAKE_USER_ID,
+} from "./constants";
 
-const FAKE_USER_ID = 1;
-const FAKE_IMAGE_EXTENSION = FAKE_USER_ID + "/" + uniqueId.makeId();
-const FAKE_POST_TITLE = faker.lorem.sentence();
-const FAKE_POST_CONTENT = faker.lorem.paragraph(3);
-const FAKE_IMAGE_CDN =
-  process.env.VITE_SUPABASE_TEST_BASE_CDN_URL + FAKE_IMAGE_EXTENSION;
+// Make the object that will be passed into post entity factory function
+export function makeFakeRawPost(overrides) {
+  const post = {
+    userId: FAKE_USER_ID,
+    title: FAKE_POST_TITLE,
+    postContent: FAKE_POST_CONTENT,
+    imgCdn: FAKE_IMAGE_CDN,
+  };
+  return { ...post, ...overrides };
+}
 
+// Make the object that will be returned by the post entity factory function
 export function makeFakePost(overrides) {
   let imgCdn =
     overrides && overrides.hasOwnProperty("imgCdn")
@@ -37,14 +43,4 @@ export function makeFakePost(overrides) {
   };
 
   return post;
-}
-
-export function makeFakeRawPost(overrides) {
-  const post = {
-    userId: FAKE_USER_ID,
-    title: FAKE_POST_TITLE,
-    postContent: FAKE_POST_CONTENT,
-    imgCdn: FAKE_IMAGE_CDN,
-  };
-  return { ...post, ...overrides };
 }
