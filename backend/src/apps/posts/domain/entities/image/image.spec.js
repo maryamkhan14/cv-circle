bb8c673ff52562d46d636ade54f3d23762f48d32
Maryam Khan <kmaryam818@gmail.com>
2023-06-27 14:46:10 -0700

    Added unit test for case of generating image entity when there is no extension provided

--- backend/src/apps/posts/domain/entities/image/image.spec.js
+++ backend/src/apps/posts/domain/entities/image/image.spec.js
@@ -1,10 +1,22 @@
-import { describe, expect, test } from "vitest";
-import { makeFakeImageEntity as makeFakeImage } from "../../../__test__/fixtures/image";
+import { describe, expect, test, vi } from "vitest";
+import {
+  makeFakeImageEntity as makeFakeImage,
+  makeFakeRawImage,
+} from "../../../__test__/fixtures/image";
+import buildMakeImage from "./image";
 import makeImage from ".";
 
 describe("Image entity", () => {
+  test("successfully generates image entity when no extension provided", () => {
+    const image = makeFakeRawImage({ extension: null });
+    const cdnGenerator = vi.fn(() => "Arbitrary CDN");
+    const extensionGenerator = vi.fn(() => "Arbitrary extension");
+    makeImage = buildMakeImage({ cdnGenerator, extensionGenerator });
+    const actual = makeImage(image);
+    expect(actual.getExtension()).toEqual("Arbitrary extension");
+  });
   test("must have image data", () => {
-    const image = makeFakeImage({ imageData: null });
+    const image = makeFakeRawImage({ imageData: null });
     expect(() => makeImage(image)).toThrow("Image must have image data.");
   });
   test("can set extension", () => {
