235271632f8a67cc8e2979431c448bf47b66720f
maryamkhan14 <kmaryam818@gmail.com>
2023-07-21 14:26:26 -0700

    Renamed cache-client to cache-store

---
+++ backend/src/apps/posts/data-access/cache-store.js
@@ -0,0 +1,10 @@
+import { createClient } from "redis";
+import RedisStore from "connect-redis";
+
+let redisClient = createClient({
+  url: process.env.REDIS_URL,
+  port: process.env.REDIS_PORT,
+});
+redisClient.connect().catch(console.error);
+let redisStore = new RedisStore({ client: redisClient, prefix: "" });
+export default redisStore;
