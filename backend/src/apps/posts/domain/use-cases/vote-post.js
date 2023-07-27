e83dfc6c12273f5c733b5a19f6fa69af582120e6
maryamkhan14 <kmaryam818@gmail.com>
2023-07-27 08:11:23 -0700

    Added vote-post use case

---
+++ backend/src/apps/posts/domain/use-cases/vote-post.js
@@ -0,0 +1,13 @@
+import makeVote from "../entities/vote";
+export default function makeVotePost({ votesDb }) {
+  return async function votePost(voteDetails) {
+    const vote = makeVote(voteDetails);
+    let { error } = await votesDb.vote(vote);
+    if (error) {
+      throw new Error(
+        `Error voting on post: ${error.message}. Post vote failed.`
+      );
+    }
+    return { ...vote.getDTO() };
+  };
+}
---
+++ backend/src/apps/posts/domain/use-cases/vote-post.spec.js
@@ -0,0 +1,33 @@
+import { describe, expect, vi, test, beforeEach } from "vitest";
+import makeVotePost from "./vote-post";
+import { makeFakeRawVote } from "../../../__test__/fixtures/vote.js";
+describe("Vote post use case", () => {
+  let votesDb;
+  let votePost;
+  beforeEach(() => {
+    let vote = vi.fn(async (voteDetails) => {
+      return { data: null, error: null };
+    });
+    votesDb = { vote };
+    votePost = makeVotePost({ votesDb });
+  });
+  test("Votes on post successfully", async () => {
+    let vote = makeFakeRawVote();
+    let expected = vote;
+    let actual = await votePost(vote);
+    let votesDbVoteArgs = votesDb.votes.mock.calls[0][0];
+    expect(actual).toEqual(expected);
+    expect(votesDb.vote).toBeCalledTimes(1);
+    expect(vote).toContain(votesDbVoteArgs);
+  });
+  test("Throws error when database save fails", async () => {
+    let vote = makeFakeRawVote();
+    let error = { message: "Vote save error message" };
+    votesDb.vote.mockImplementation(async () => {
+      return { data: null, error };
+    });
+    expect(votePost(vote)).rejects.toThrow(
+      `Error voting on post: ${error.message}. Post vote failed.`
+    );
+  });
+});
