02011a30eabc6129587fd64588a5c6a127ba1b38
maryamkhan14 <kmaryam818@gmail.com>
2023-07-27 08:28:46 -0700

    Added controller and routing for POST to /vote endpoint

--- backend/src/apps/posts/entry-points/api/post-controller.js
+++ backend/src/apps/posts/entry-points/api/post-controller.js
@@ -11,19 +11,29 @@ import makeGetAllPosts from "./get/get-all-posts.js";
 import makeGetSinglePost from "./get/get-single-post.js";
 import makePatchPost from "./patch/patch-post.js";
 import makeDeletePost from "./delete/delete-post.js";
-
+import makePostVote from "./vote/vote-post.js";
 const postPost = makePostPost({ createPost, handleAttachmentPreview });
 const getAllPosts = makeGetAllPosts({ retrievePosts });
 const getSinglePost = makeGetSinglePost({ retrieveSinglePost });
 const patchPost = makePatchPost({ updatePost, handleAttachmentPreview });
 const deletePost = makeDeletePost({ retrieveSinglePost, removePost });
+const postVote = makePostVote({ votePost });
+
 const postController = Object.freeze({
   postPost,
   getAllPosts,
   getSinglePost,
   patchPost,
   deletePost,
+  postVote,
 });
 
 export default postController;
-export { postPost, getAllPosts, getSinglePost, patchPost, deletePost };
+export {
+  postPost,
+  getAllPosts,
+  getSinglePost,
+  patchPost,
+  deletePost,
+  postVote,
+};
