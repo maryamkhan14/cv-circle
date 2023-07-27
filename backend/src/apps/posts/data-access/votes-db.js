cdf04940d345a971de2e28bd5430ec8a25256ba8
maryamkhan14 <kmaryam818@gmail.com>
2023-07-27 07:35:23 -0700

    Added vote entity and upvote, downvote methods in votes-db

---
+++ backend/src/apps/posts/data-access/votes-db.js
@@ -0,0 +1,36 @@
+export default function makeVotesDb({ dbClient }) {
+  const normalizedProfileToDbColumns = {
+    voteCount: "vote_count",
+    userId: "fk_uid",
+    postId: "fk_pid",
+  };
+
+  function renameKeys(obj, keysMap) {
+    return Object.keys(obj).reduce(
+      (acc, key) => ({
+        ...acc,
+        ...{ [keysMap[key] || key]: obj[key] },
+      }),
+      {}
+    );
+  }
+
+  async function upvote(voteDetails) {
+    let result = await dbClient.rpc("vote", {
+      ...renameKeys(voteDetails, normalizedProfileToDbColumns),
+    });
+    return { ...result };
+  }
+
+  async function downvote(voteDetails) {
+    let result = await dbClient.rpc("vote", {
+      ...renameKeys(voteDetails, normalizedProfileToDbColumns),
+    });
+    return { ...result };
+  }
+
+  return Object.freeze({
+    upvote,
+    downvote,
+  });
+}
---
+++ backend/src/apps/posts/domain/entities/vote/index.js
@@ -0,0 +1,4 @@
+import buildMakeVote from "./vote";
+const makeVote = buildMakeVote();
+
+export default makeVote;
