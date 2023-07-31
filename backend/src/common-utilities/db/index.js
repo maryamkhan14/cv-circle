727287d716c7ab691427f4a8f1a4fbe7f4d41ba6
maryamkhan14 <kmaryam818@gmail.com>
2023-07-31 15:15:51 -0700

    Removed unnecessary files

--- backend/src/common-utilities/db/index.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = process.env.VITE_SUPABASE_URL;
-const supabaseKey = process.env.VITE_SUPABASE_KEY;
-
-const dbClient = createClient(supabaseUrl, supabaseKey);
-export default dbClient;
