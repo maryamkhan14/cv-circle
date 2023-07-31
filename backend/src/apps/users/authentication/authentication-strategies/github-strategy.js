111a08f771fbd57512b22ae1cb0fbd7acf14067d
maryamkhan14 <kmaryam818@gmail.com>
2023-07-31 13:18:42 -0700

    Testing removal of HTTPS from github authorization strategy

--- backend/src/apps/users/authentication/authentication-strategies/github-strategy.js
+++ backend/src/apps/users/authentication/authentication-strategies/github-strategy.js
@@ -6,7 +6,7 @@ export default function buildGithubStrategy({
     {
       clientID: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      callbackURL: `https://cv-circle.com/api/auth${process.env.GITHUB_CALLBACK_URL}`,
+      callbackURL: `/api/auth${process.env.GITHUB_CALLBACK_URL}`,
     },
     verificationCallback
   );
