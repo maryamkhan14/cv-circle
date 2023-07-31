20f57b8d1b830f752f6fcd8c70fb5d5509accfcc
maryamkhan14 <kmaryam818@gmail.com>
2023-07-31 10:48:04 -0700

    Set up Express servers for receiving proxied requests

--- backend/src/apps/posts/entry-points/server.js
+++ backend/src/apps/posts/entry-points/server.js
@@ -8,12 +8,14 @@ import { sessionDetailsConsumer } from "./message-queue/index.js";
 import { cacheStore } from "../data-access/index.js";
 
 const app = express();
+app.enable("trust proxy");
 app.use(express.json());
 app.use(
   session({
     store: cacheStore,
     secret: process.env.SESSION_SECRET,
     resave: false,
+    proxy: true,
     saveUninitialized: false,
   })
 );
--- backend/src/apps/users/entry-points/server.js
+++ backend/src/apps/users/entry-points/server.js
@@ -9,13 +9,14 @@ import configuredPassportInstance from "../authentication/passport-setup/index.j
 let redisStore = new RedisStore({ client: redisClient, prefix: "sess:" });
 
 const app = express();
-
+app.enable("trust proxy");
 app.use(express.json());
 app.use(
   session({
     store: redisStore,
     secret: process.env.SESSION_SECRET,
     resave: false,
+    proxy: true,
     cookie: { maxAge: 86400000 },
     saveUninitialized: false,
   })
