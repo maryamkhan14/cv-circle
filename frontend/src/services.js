2acd963332ac6a91db8d147c81cc49abcf44c897
maryamkhan14 <kmaryam818@gmail.com>
2023-08-08 13:35:34 -0700

    Remove frontend services.js file from gitignore Maintain one file but use conditional expressions to determine appropriate domains based on environment

---
+++ frontend/src/services.js
@@ -0,0 +1,101 @@
+import axios from "axios";
+axios.defaults.withCredentials = true;
+const DOMAIN = import.meta.env.DEV
+  ? "http://localhost"
+  : "https://cv-circle.com";
+const getPost = async (postId) => {
+  try {
+    return await axios.get(`${DOMAIN}/api/posts/${postId}`, {
+      withCredentials: true,
+    });
+  } catch (e) {
+    let { data, status } = e.response;
+    let errorMsg = data.error;
+    return { error: errorMsg, status };
+  }
+};
+const getAllPosts = async () => {
+  try {
+    return await axios.get(`${DOMAIN}/api/posts/`, {
+      withCredentials: true,
+    });
+  } catch (e) {
+    let { data, status } = e.response;
+    let errorMsg = data.error;
+    return { error: errorMsg, status };
+  }
+};
+
+const updatePost = async (post, id) => {
+  try {
+    let { data: updatedPost } = await axios.patch(
+      `${DOMAIN}/api/posts/${id}`,
+      post,
+      { headers: { "Content-Type": "multipart/form-data" } }
+    );
+    return updatedPost;
+  } catch (e) {
+    console.log(e);
+    let { data, status, statusText } = e.response;
+    let errorMsg = data.error;
+    return { error: errorMsg || statusText, status };
+  }
+};
+
+const createPost = async (post) => {
+  try {
+    let { data: newPost } = await axios.post(`${DOMAIN}/api/posts/`, post, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+    return newPost;
+  } catch (e) {
+    let { data, status } = e.response;
+    let errorMsg = data.error;
+    return { error: errorMsg, status };
+  }
+};
+const votePost = async (vote) => {
+  try {
+    let { data: voted } = await axios.post(`${DOMAIN}/api/posts/vote`, vote, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+    return voted;
+  } catch (e) {
+    let { data, status } = e.response;
+    let errorMsg = data.error;
+    return { error: errorMsg, status };
+  }
+};
+const deletePost = async (postId) => {
+  try {
+    let { data: deletedPost } = await axios.delete(
+      `${DOMAIN}/api/posts/${postId}`
+    );
+    return deletedPost;
+  } catch (e) {
+    console.log(e);
+    let { data, status, statusText } = e.response;
+    let errorMsg = data.error;
+    return { error: errorMsg || statusText, status };
+  }
+};
+const AUTH_URL_GITHUB = `${DOMAIN}/api/auth/github`;
+const AUTH_URL_GOOGLE = `${DOMAIN}/api/auth/google`;
+const AUTH_SUCCESS_URL = `${DOMAIN}/api/auth/success`;
+const AUTH_LOGOUT_URL = `${DOMAIN}/api/auth/logout`;
+export {
+  createPost,
+  updatePost,
+  getPost,
+  getAllPosts,
+  deletePost,
+  votePost,
+  AUTH_URL_GITHUB,
+  AUTH_URL_GOOGLE,
+  AUTH_SUCCESS_URL,
+  AUTH_LOGOUT_URL,
+};
