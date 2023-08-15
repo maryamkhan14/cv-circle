d7180a4eba210e88017c082d722876892df7461c
maryamkhan14 <kmaryam818@gmail.com>
2023-08-15 07:54:07 -0700

    Fix test coverage reporting - Modify vitest config files to output coverage to correct directory - Set up bind mount between container coverage directory and host machine - Add coverage to gitignore - Add coverage script in package.json files

--- backend/src/apps/posts/__test__/vitest.config.js
+++ backend/src/apps/posts/__test__/vitest.config.js
@@ -2,13 +2,14 @@ import { defineConfig } from "vitest/config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  setupFiles: "/__test__",
-  globalSetup: "/__test__/globalSetup.ts",
   test: {
     root: ".",
     coverage: {
       provider: "istanbul", // or 'v8'
       reporter: ["html"],
+      reportsDirectory: "./__test__/coverage",
+      cleanOnRerun: false,
+      clean: false,
     },
   },
 });
