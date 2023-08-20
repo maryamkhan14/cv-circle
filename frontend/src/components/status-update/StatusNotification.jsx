21a91f664e826098776bc6e6c2ddff1782424dfa
maryamkhan14 <kmaryam818@gmail.com>
2023-08-20 08:58:49 -0700

    Update StatusNotification to match format of status used by React Query - Match status to 'loading', 'error', or idle - Get status message as prop - Use 'popup' prop to make styling decision

--- frontend/src/components/status-update/StatusNotification.jsx
+++ frontend/src/components/status-update/StatusNotification.jsx
@@ -1,17 +1,19 @@
 import { useState, useEffect } from "react";
 import NotificationButtons from "./NotificationButtons";
-const StatusNotification = ({ status }) => {
+const StatusNotification = ({ status, msg, popup }) => {
   const [show, setShow] = useState(false);
   useEffect(() => {
-    if (status.msg) {
+    if (msg) {
       setShow(true);
     }
-  }, [status]);
+  }, [msg]);
   return (
     show &&
-    (status?.error ? (
+    (status === "error" ? (
       <div
-        className="lg:!fixed bottom-5 right-5 flex items-center p-4 my-4 text-sm text-red-800 border border-red-300 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400 dark:border-red-800"
+        className={`${
+          popup && "lg:!fixed"
+        } bottom-5 right-5 self-center flex items-center p-4 my-4 text-sm text-red-800 border border-red-300 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400 dark:border-red-800`}
         role="alert"
       >
         <svg
@@ -25,12 +27,12 @@ const StatusNotification = ({ status }) => {
         </svg>
         <span className="sr-only">Info</span>
         <div>
-          <span className="font-medium">Status: </span> {status?.msg}
+          <span className="font-medium">Status: </span> {msg}
         </div>
 
         <NotificationButtons setShow={setShow} />
       </div>
-    ) : status?.success == 1 ? (
+    ) : status === "loading" ? (
       <div
         className="lg:!fixed bottom-5 right-5  flex items-center p-4 my-4 text-sm text-blue-800 border border-blue-300 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400 dark:border-blue-800"
         role="alert"
@@ -53,32 +55,34 @@ const StatusNotification = ({ status }) => {
         </svg>
         <span className="sr-only">Info</span>
         <div>
-          <span className="font-medium">Status: </span> {status?.msg}
+          <span className="font-medium">Status: </span> {msg}
         </div>
 
         <NotificationButtons setShow={setShow} />
       </div>
     ) : (
-      <div
-        className="lg:!fixed bottom-5 right-5  flex items-center p-4 my-4 text-sm text-blue-800 border border-blue-300 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400 dark:border-blue-800"
-        role="alert"
-      >
-        <svg
-          className="flex-shrink-0 inline w-4 h-4 mr-3"
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="currentColor"
-          viewBox="0 0 20 20"
+      status === "success" && (
+        <div
+          className="lg:!fixed bottom-5 right-5  flex items-center p-4 my-4 text-sm text-blue-800 border border-blue-300 rounded-lg bg-blue-50 dark:bg-gray-800 dark:text-blue-400 dark:border-blue-800"
+          role="alert"
         >
-          <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
-        </svg>
-        <span className="sr-only">Info</span>
-        <div>
-          <span className="font-medium">Status: </span> {status?.msg}
-        </div>
+          <svg
+            className="flex-shrink-0 inline w-4 h-4 mr-3"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+          >
+            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+          </svg>
+          <span className="sr-only">Info</span>
+          <div>
+            <span className="font-medium">Status: </span> {msg}
+          </div>
 
-        <NotificationButtons setShow={setShow} />
-      </div>
+          <NotificationButtons setShow={setShow} />
+        </div>
+      )
     ))
   );
 };
