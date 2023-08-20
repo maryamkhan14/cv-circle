c6e38893613c1de9bac104b8ab6be54b6dd3b023
maryamkhan14 <kmaryam818@gmail.com>
2023-08-20 14:52:08 -0700

    Add override option to useUser hook - Return value can be overridden if an object is passed as an argument

--- frontend/src/features/authentication/hooks/useUser.js
+++ frontend/src/features/authentication/hooks/useUser.js
@@ -1,11 +1,12 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { getCurrentUser } from "../services";
 
-export default function useUser() {
+export default function useUser(overrides = {}) {
   return useQuery({
     queryKey: ["user"],
     queryFn: async () => {
-      return await getCurrentUser();
+      const user = await getCurrentUser();
+      return { ...user, ...overrides };
     },
     staleTime: 60 * 1000 * 60 * 24, // 24 hours,
     refetchOnWindowFocus: false,
