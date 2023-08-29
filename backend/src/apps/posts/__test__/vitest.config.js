9d4e657b30f2fd08dfd43bfd4b7afbbf783a3b4f
maryamkhan14 <kmaryam818@gmail.com>
2023-08-28 20:03:42 -0700

    Include untested files in posts feature's code coverage (excluding files that don't need tests) - Add 'exclude' and 'coverage: all' properties in vitest configuration file

--- backend/src/apps/posts/__test__/vitest.config.js
+++ backend/src/apps/posts/__test__/vitest.config.js
@@ -5,6 +5,14 @@ export default defineConfig({
   test: {
     root: ".",
     coverage: {
+      exclude: [
+        "__test__/**",
+        "authentication/**",
+        "domain/services/authenticator/**",
+        "message-producer/**",
+        "**/index.js",
+      ],
+      all: true,
       provider: "istanbul", // or 'v8'
       reporter: ["html"],
       reportsDirectory: "./__test__/coverage",
