f645560260d24ca4feda63e728c364ffce133fcc
maryamkhan14 <kmaryam818@gmail.com>
2023-08-28 20:20:42 -0700

    Improve votePost use case tests and test coverage - Ensure all mocks are restored after each test - Change 'test' to 'it' to match convention - Use Vitest's mocking utilities instead of manually mocking modules

--- backend/src/apps/posts/domain/use-cases/vote-post.spec.js
+++ backend/src/apps/posts/domain/use-cases/vote-post.spec.js
@@ -1,31 +1,28 @@
-import { describe, expect, vi, test, beforeEach } from "vitest";
-import makeVotePost from "./vote-post";
+import { describe, expect, vi, it, afterEach } from "vitest";
+import { votePost } from ".";
+import { votesDb } from "../../data-access";
 import { makeFakeRawVote } from "../../__test__/fixtures/vote.js";
-describe("Vote post use case", () => {
-  let votesDb;
-  let votePost;
-  beforeEach(() => {
-    let vote = vi.fn(async (voteDetails) => {
-      return { data: null, error: null };
-    });
-    votesDb = { vote };
-    votePost = makeVotePost({ votesDb });
+vi.mock("../../data-access");
+
+describe("Vote post use case tests", () => {
+  afterEach(() => {
+    vi.restoreAllMocks();
   });
-  test("Votes on post successfully", async () => {
+  it("votes on post successfully", async () => {
+    votesDb.vote.mockResolvedValue({ data: null, error: null });
     let vote = makeFakeRawVote();
     let expected = vote;
     let actual = await votePost(vote);
-    let votesDbVoteArgs = votesDb.votes.mock.calls[0][0];
+    let votesDbVoteArgs = votesDb.vote.mock.calls[0][0];
     expect(actual).toEqual(expected);
     expect(votesDb.vote).toBeCalledTimes(1);
     expect(vote).toContain(votesDbVoteArgs);
   });
-  test("Throws error when database save fails", async () => {
+  it("throws error when database save fails", async () => {
     let vote = makeFakeRawVote();
     let error = { message: "Vote save error message" };
-    votesDb.vote.mockImplementation(async () => {
-      return { data: null, error };
-    });
+    votesDb.vote.mockResolvedValue({ data: null, error });
+
     expect(votePost(vote)).rejects.toThrow(
       `Error voting on post: ${error.message}. Post vote failed.`
     );
