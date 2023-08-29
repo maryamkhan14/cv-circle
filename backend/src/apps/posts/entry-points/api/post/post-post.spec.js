db4737d2db8f4d76acd7dccbc0e4f4536d2fd177
maryamkhan14 <kmaryam818@gmail.com>
2023-08-28 20:28:23 -0700

    Improve postPost controller tests and test coverage - Add unit test for case of no file, and unit test for error response - Ensure all mocks are restored after each test - Change 'test' to 'it' to match convention - Use Vitest's mocking utilities instead of manually mocking modules

--- backend/src/apps/posts/entry-points/api/post/post-post.spec.js
+++ backend/src/apps/posts/entry-points/api/post/post-post.spec.js
@@ -1,19 +1,22 @@
-import { describe, expect, beforeEach, vi, test } from "vitest";
+import { describe, expect, afterEach, vi, it } from "vitest";
 import { makeFakeRawPost } from "../../../__test__/fixtures/post";
-import { makeFakeImageEntity as makeFakeImage } from "../../../__test__/fixtures/image";
-import makePostPost from "./post-post";
+import { makeFakeImageEntity } from "../../../__test__/fixtures/image";
+import { postPost } from "../post-controller";
+import {
+  createPost,
+  handleAttachmentPreview,
+} from "../../../domain/use-cases/";
+vi.mock("../../../domain/use-cases/");
 
 describe("Controller for POST to /post endpoint", () => {
-  let previewImage = makeFakeImage();
-  let handleAttachmentPreview = vi.fn(async () => previewImage);
-  let createPost = vi.fn(async (post) => post);
-  let postPost;
-  beforeEach(() => {
-    postPost = makePostPost({ createPost, handleAttachmentPreview });
+  let previewImage = makeFakeImageEntity();
+  afterEach(() => {
+    vi.restoreAllMocks();
   });
-
-  test("Successfully creates a post", async () => {
+  it("successfully creates a post when new file is attached", async () => {
     const post = makeFakeRawPost();
+    createPost.mockResolvedValue(post);
+    handleAttachmentPreview.mockResolvedValue(previewImage);
     const request = {
       headers: {
         "Content-Type": "application/json",
@@ -27,6 +30,55 @@ describe("Controller for POST to /post endpoint", () => {
       },
       statusCode: 200,
       body: { posted: { ...post, imgCdn: previewImage.getCdn() } },
+    };
+    const actual = await postPost(request);
+    expect(actual).toEqual(expected);
+  });
+
+  it("successfully creates a post when no file is attached", async () => {
+    const post = makeFakeRawPost();
+    createPost.mockResolvedValue(post);
+    handleAttachmentPreview.mockResolvedValue(null);
+    const request = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: post,
+    };
+    const expected = {
+      headers: {
+        "Content-Type": "application/json",
+        "Last-Modified": new Date(request.modifiedOn).toUTCString(),
+      },
+      statusCode: 200,
+      body: { posted: { ...post } },
+    };
+    const actual = await postPost(request);
+    expect(actual).toEqual(expected);
+  });
+
+  it("returns expected response error when exception is thrown", async () => {
+    const error = {
+      message: "Error thrown by POST /api/posts/ controller",
+    };
+    handleAttachmentPreview.mockImplementation(async () => {
+      throw new Error(error.message);
+    });
+    const post = makeFakeRawPost({ file: "fakeFile" });
+    const user = { userId: post.userId };
+    const request = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: { ...post },
+      user,
+    };
+    const expected = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      statusCode: 400,
+      body: { error: error.message },
     };
     const actual = await postPost(request);
     expect(actual).toEqual(expected);
