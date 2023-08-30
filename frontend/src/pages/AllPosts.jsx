49840a3c8e4bbad13ea449bce47a36776a743da8
maryamkhan14 <kmaryam818@gmail.com>
2023-08-30 11:58:49 -0700

    Fix error status not showing up on AllPosts - Dispatch status update of fetching all posts through PostsList - Render status notification only passing popup prop - Render PostsSkeleton conditionally in PostsList

--- frontend/src/pages/AllPosts.jsx
+++ frontend/src/pages/AllPosts.jsx
@@ -2,13 +2,10 @@ import React from "react";
 import { PostsListContextProvider } from "../features/posts/context/PostsListContext";
 import { StatusContextProvider } from "../features/notifications/context/StatusContext";
 import PostsList from "../features/posts/components/list/PostsList";
-import PostsSkeleton from "../features/posts/components/PostsSkeleton";
 import StatusNotification from "../features/notifications/components/StatusNotification";
-import useAllPosts from "../features/posts/hooks/useAllPosts";
 import SearchBar from "../features/posts/components/list/SearchBar";
 import SortSelect from "../features/posts/components/list/SortSelect";
 const AllPosts = () => {
-  const { isLoading, isError, data: allPosts, error } = useAllPosts();
   return (
     <StatusContextProvider>
       <PostsListContextProvider>
@@ -17,13 +14,8 @@ const AllPosts = () => {
             <SearchBar />
             <SortSelect />
           </span>
-          {isLoading ? (
-            <PostsSkeleton />
-          ) : isError ? (
-            <StatusNotification status={"error"} msg={error.message} />
-          ) : (
-            <PostsList allPosts={allPosts} />
-          )}
+          <PostsList />
+          <StatusNotification popup={"true"} />
         </div>
       </PostsListContextProvider>
     </StatusContextProvider>
