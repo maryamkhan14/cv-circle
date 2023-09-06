c7b25f9fd8808eda6bb6d1597d8809a00b49cc7f
maryamkhan14 <kmaryam818@gmail.com>
2023-09-06 11:34:32 -0700

    Display post author on each post - Use React Suspense to lazy load post author details - Create thumbnail and dropdown components - Update styles

---
+++ frontend/src/features/posts/components/PostAuthorDropdown.jsx
@@ -0,0 +1,36 @@
+import { Link } from "react-router-dom";
+import LoadingSvg from "../assets/LoadingSvg";
+const PostAuthorDropdown = ({ author, status }) => {
+  return status === "loading" ? (
+    <LoadingSvg />
+  ) : status === "success" ? (
+    <>
+      <div className="px-4 py-3 flex flex-col min-w-[10em] items-between gap-3 justify-center text-center">
+        <h3 className="break-words break-all font-medium">
+          {author?.displayName || author?.name}
+        </h3>
+        <h4 className="text-sm italic leading-tight font-medium hyphens-auto break-words w-full">
+          {author?.bio || "This user has not set a bio."}
+        </h4>
+      </div>
+      <ul className="py-1 text-center" aria-labelledby="dropdown">
+        <li>
+          <Link
+            to="/profile"
+            className="text-sm hover:bg-gray-100 text-gray-700 block px-4 py-2"
+          >
+            <h3 className="text-amber-800">View full profile</h3>
+          </Link>
+        </li>
+      </ul>
+    </>
+  ) : (
+    status === "error" && (
+      <div className="px-4 py-3 flex flex-col min-w-[10em] items-between gap-3">
+        <p>Error loading user.</p>
+      </div>
+    )
+  );
+};
+
+export default PostAuthorDropdown;
--- frontend/src/features/posts/components/PostDetails.jsx
+++ frontend/src/features/posts/components/PostDetails.jsx
@@ -1,28 +1,20 @@
-import React from "react";
-import { useEffect, useState, useContext } from "react";
-import { usePost } from "../hooks";
-import {
-  Link,
-  useParams,
-  useNavigate,
-  useOutletContext,
-} from "react-router-dom";
-import VoteDisplay from "../../votes/components/VoteDisplay";
-import PostSkeleton from "./PostSkeleton";
-import ReplyList from "../../replies/components/ReplyList";
-import ReplyForm from "../../replies/components/form/ReplyForm";
-import StatusNotification from "../../notifications/components/StatusNotification";
+import React, { lazy, Suspense, useEffect, useContext } from "react";
 import { StatusContext } from "../../notifications/context/StatusContext";
 import { ReplyFormContext } from "../../replies/context/ReplyFormContext";
+import { usePost } from "../hooks";
+import { Link } from "react-router-dom";
+import VoteDisplay from "../../votes/components/VoteDisplay";
 import PostOptions from "./PostOptions";
+import ReplyForm from "../../replies/components/form/ReplyForm";
+import ReplyList from "../../replies/components/ReplyList";
+import StatusNotification from "../../notifications/components/StatusNotification";
+import PostSkeleton from "./PostSkeleton";
+import LoadingSvg from "../assets/LoadingSvg";
+const PostAuthor = lazy(() => import("./PostAuthor"));
 const PostDetails = ({ postId, updated, user }) => {
   let { status: postStatus, data: post, error } = usePost(postId);
-  const { replyForm, dispatch: replyFormDispatch } =
-    useContext(ReplyFormContext);
-  const { status, dispatch } = useContext(StatusContext);
-  const handleReplyClick = () => {
-    replyFormDispatch({ type: "SWITCH_REPLY_FORM_ACTIVE" });
-  };
+  const { replyForm } = useContext(ReplyFormContext);
+  const { dispatch } = useContext(StatusContext);
   useEffect(() => {
     if (error) {
       dispatch({
@@ -44,14 +36,23 @@ const PostDetails = ({ postId, updated, user }) => {
         post && (
           <>
             <span className="flex flex-col md:gap-10 ">
-              <span className="flex flex-col md:flex-row gap-5 pb-3 border-b border-slate-300 items-center">
+              <span className="flex flex-wrap md:flex-nowrap gap-5 pb-3 border-b border-slate-300 items-center w-full h-full justify-between">
                 <VoteDisplay
                   postId={post.id}
                   existingUpvoteCount={post.upvoteCount}
                 />
-                <h1 className="text-4xl font-semibold text-slate-900">
+                <h1 className="text-4xl font-semibold text-slate-900 order-2 w-full md:grow md:order-none">
                   {post.title}
                 </h1>
+                <Suspense
+                  fallback={
+                    <div className="w-16 h-16">
+                      <LoadingSvg />
+                    </div>
+                  }
+                >
+                  <PostAuthor authorId={post.userId} currentUser={user} />
+                </Suspense>
               </span>
               <span className="flex flex-col pt-3 md:pt-0 md:flex-row gap-3 justify-between">
                 <p
