0a077672b358187abb3428417d5dc6172d04960c
maryamkhan14 <kmaryam818@gmail.com>
2023-09-08 14:20:01 -0700

    Lazily load ReplyChain in ReplyList component - Update imports and add Suspense tags where boundaries for ReplyChain, since it may not be needed

--- frontend/src/features/replies/components/ReplyList.jsx
+++ frontend/src/features/replies/components/ReplyList.jsx
@@ -1,23 +1,27 @@
+import { Suspense } from "react";
+import LoadingSvg from "../../notifications/assets/LoadingSvg";
 import ReplyChain from "./ReplyChain";
 const ReplyList = ({ replies, user, originalAuthorId }) => {
   return (
     <div className="flex flex-col border-t border-slate-300 py-3">
       {Object.keys(replies).length ? (
-        <div>
-          <h3 className="text-3xl font-semibold text-slate-900 mb-5">
-            Insights
-          </h3>
-          {Object.keys(replies).map((chainId) => {
-            return (
-              <ReplyChain
-                reply={replies[chainId]}
-                key={chainId}
-                user={user}
-                originalAuthorId={originalAuthorId}
-              />
-            );
-          })}
-        </div>
+        <Suspense fallback={<LoadingSvg />}>
+          <div>
+            <h3 className="text-3xl font-semibold text-slate-900 mb-5">
+              Insights
+            </h3>
+            {Object.keys(replies).map((chainId) => {
+              return (
+                <ReplyChain
+                  reply={replies[chainId]}
+                  key={chainId}
+                  user={user}
+                  originalAuthorId={originalAuthorId}
+                />
+              );
+            })}
+          </div>
+        </Suspense>
       ) : (
         <div className="flex justify-center m-3 italic">
           No replies yet. {user ? "Chime in!" : "Log in to add your own!"}
