be7797be4cad0c2aaabb5cc883a77390c0f95db3
maryamkhan14 <kmaryam818@gmail.com>
2023-09-10 19:13:58 -0700

    Create InteractiveContext for managing form interactivity, replacing StatusContext

---
+++ frontend/src/context/InteractiveContext.jsx
@@ -0,0 +1,27 @@
+import { createContext, useContext, useReducer } from "react";
+export const InteractiveContext = createContext();
+export const InteractiveDispatchContext = createContext();
+export const InteractiveContextReducer = (state, action) => {
+  switch (action.type) {
+    case "TOGGLE_INTERACTIVE":
+      return !state.interactive;
+    case "UPDATE_INTERACTIVE":
+      return action.payload !== "loading" ? true : false;
+    default:
+      return state;
+  }
+};
+export const InteractiveContextProvider = ({ children }) => {
+  const [interactive, dispatch] = useReducer(InteractiveContextReducer, true);
+
+  return (
+    <InteractiveContext.Provider value={interactive}>
+      <InteractiveDispatchContext.Provider value={dispatch}>
+        {children}
+      </InteractiveDispatchContext.Provider>
+    </InteractiveContext.Provider>
+  );
+};
+export const useInteractive = () => useContext(InteractiveContext);
+export const useInteractiveDispatch = () =>
+  useContext(InteractiveDispatchContext);
